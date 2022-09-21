39c6925b875bcdc66601a17bfdd60bdf78d3714d
sebastien.vanpe@tobania.be <Born2bealive$>
2022-09-21 15:18:43 +0200

    quarkus ready for demo

--- discount-api-quarkus/src/main/java/be/tobania/fast/and/furious/discount/DiscountRuleServiceImpl.java
+++ discount-api-quarkus/src/main/java/be/tobania/fast/and/furious/discount/DiscountRuleServiceImpl.java
@@ -4,6 +4,7 @@ import be.tobania.fast.and.furious.discount.model.DiscountRule;
 import be.tobania.fast.and.furious.discount.rules.CustomerRewardsRule;
 import be.tobania.fast.and.furious.discount.rules.OnePlusOneFree;
 import be.tobania.fast.and.furious.discount.rules.RedPriceAdjustmentRule;
+//import be.tobania.fast.and.furious.discount.rules.TheSecondIsHalfPrice;
 
 
 import javax.enterprise.context.ApplicationScoped;
@@ -21,11 +22,12 @@ public class DiscountRuleServiceImpl implements DiscountRuleService {
     private CustomerRewardsRule customerRewardsRule;
 
 
+
     @Override
     public List<DiscountRule> loadDiscountRules() {
         List<DiscountRule> rules = new ArrayList<>();
         rules.add(new OnePlusOneFree("COKE4553334"));
-       // rules.add(new TheSecondIsHalfPrice("FAT34333005"));
+       // rules.add(new TheSecondIsHalfPrice("COKE4553334"));
         rules.add(customerRewardsRule);
        rules.add(redPriceAdjustmentRule);
         return rules;
---
+++ discount-api-springnative/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into customer_rewards_entity (customer_identifier,customer_points) values ('0000000001',400);
+insert into customer_rewards_entity (customer_identifier,customer_points) values ('0000000002',501);
+insert into customer_rewards_entity (customer_identifier,customer_points) values ('0000000003',1001);
